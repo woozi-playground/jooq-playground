8da9f3f69958197ebba65cf02ddddc847429f820
woozi <kwj1270@naver.com>
2024-06-16 15:17:25 +0900

    feat: TestContainer 베이스 DSL Generator

---
+++ entity/src/test/java/org/woozi/pratice/entity/EntityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.woozi.pratice.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
